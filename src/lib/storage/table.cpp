c01360fe2fb40ae14042daa12e68d26b4cadbfab
Richard Ebeling <klavier22@hotmail.de>
2019-10-21 17:32:52 +0200

    Progres...

--- src/lib/storage/table.cpp
+++ src/lib/storage/table.cpp
@@ -26,28 +26,34 @@ void Table::append(std::vector<AllTypeVariant> values) {
 }
 
 uint16_t Table::column_count() const {
-  // Implementation goes here
-  return 0;
+  // At creation of the table, a chunk is created. It is not possible to delete a chunk (only to emplace it).
+  // Thus, _chunks[0] should always be valid.
+  return _chunks[0].column_count();
 }
 
 uint64_t Table::row_count() const {
-  // Implementation goes here
-  return 0;
+  // TODO: static_cast necessary here?
+  return std::accumulate(_chunks.cbegin(), _chunks.cend(), static_cast<uint64_t>(0), [](uint64_t sum, const Chunk& chunk){
+      return sum + chunk.size();
+  });
 }
 
 ChunkID Table::chunk_count() const {
-  // Implementation goes here
-  return ChunkID{0};
+  // ChunkID is uint32_t, the vector could theoretically be as big as a uint64_t can become.
+  return static_cast<ChunkID>(_chunks.size());
 }
 
 ColumnID Table::column_id_by_name(const std::string& column_name) const {
-  // Implementation goes here
-  return ColumnID{0};
+  auto column_it = std::find(column_names.cbegin(), column_names.cend(), column_name);
+  if (column_it == column_names.cend ()) {
+    throw std::runtime_error("Invalid column name");
+  }
+
+  return std::distance(column_names.cbegin(), column_it);
 }
 
 uint32_t Table::max_chunk_size() const {
-  // Implementation goes here
-  return 0;
+  return _max_chunk_size;
 }
 
 const std::vector<std::string>& Table::column_names() const {
